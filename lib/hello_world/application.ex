40f2fd08179ed717018ac45150f3be10899ca073
Brendan A R Sechter <bsechter@sennue.com>
2015-12-25 00:15:10 +0900

    Using a more general and potentially easier to test solution in lib/hello_world/application.ex.

--- lib/hello_world/application.ex
+++ lib/hello_world/application.ex
@@ -3,8 +3,12 @@ defmodule HelloWorld.Application do
 
   def start(_type, _args) do
     {:ok, pid} = Task.Supervisor.start_link()
-    Task.Supervisor.async(pid, HelloWorld, :main, [["--name=#{"USER" |> System.get_env }"]])
+    Task.Supervisor.async(pid, __MODULE__, :main, [args])
     {:ok, pid}
   end
+
+  def main([]), do: main ["--name=#{"USER" |> System.get_env }"]
+  def main(args), do: Mix.Project.get.project[:escript][:main_module].main(args)
+  def args(), do: System.argv
 end
 
