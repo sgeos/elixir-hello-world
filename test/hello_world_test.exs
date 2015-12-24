d2acc3ec325c70ad67673c00480361a10b20daf2
Brendan A R Sechter <bsechter@sennue.com>
2015-12-25 02:11:26 +0900

    Added application tests.

--- test/hello_world_test.exs
+++ test/hello_world_test.exs
@@ -20,10 +20,22 @@ defmodule HelloWorldTest do
     end) == "Hello, User!\n"
   end
 
-  #test "application" do
-  #  assert capture_io(fn ->
-  #    HelloWorld.Application.start(nil, nil)
-  #  end) == "Hello, #{"USER" |> System.get_env}!\n"
-  #end
+  test "application hello world" do
+    assert capture_io(fn ->
+      HelloWorld.Application.main []
+    end) == "Hello, World!\n"
+  end
+
+  test "application hello user" do
+    assert capture_io(fn ->
+      HelloWorld.Application.main ["--name=User"]
+    end) == "Hello, User!\n"
+  end
+
+  test "application terminates" do
+    {:ok, pid} = HelloWorld.Application.start(nil, nil)
+    %Task{ref: ref} = HelloWorld.Application.main_task(pid)
+    assert_receive {:DOWN, ^ref, :process, _, :normal}, 100
+  end
 end
 
