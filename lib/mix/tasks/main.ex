cddeb58c0e0ee0ae7e774f07a0752a44cc395d98
Brendan A R Sechter <bsechter@sennue.com>
2015-12-24 11:08:27 +0900

    lib/mix/tasks/main.ex uses a project agnostic solution.

--- lib/mix/tasks/main.ex
+++ lib/mix/tasks/main.ex
@@ -1,9 +1,8 @@
 defmodule Mix.Tasks.Main do
   use Mix.Task
-  alias HelloWorld.Mixfile
 
   def run(args) do
-    Mixfile.project[:escript][:main_module].main(args) 
+    Mix.Project.get.project[:escript][:main_module].main(args) 
   end
 end
 
